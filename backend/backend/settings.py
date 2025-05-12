b8ea545c808b16785344019d2bec68282f7b6ebd
Amir Daniali <amir@amirdaniali.com>
2025-05-12 11:09:31 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
